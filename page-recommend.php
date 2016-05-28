005afdd91d30d35d4dec15603a933c50264ea98c
wwbewbe <what.wbewbe@gmail.com>
2016-05-28 16:42:26 +0900

    remove page-neighborhood.php and fix page-recommend.php

--- page-recommend.php
+++ page-recommend.php
@@ -1,113 +1,141 @@
 <?php get_header(); ?>
 
-<div class="sub-header">
-	<div class="bread">
-	<?php if( has_category() ): ?>
-	<?php $postcats=get_the_category(); ?>
-	<?php foreach( $postcats as $postcat ): ?>
-	<ol>
-		<li><a href="<?php echo home_url(); ?>">
-		<i class="fa fa-home"></i><span>TOP</span>
-		</a></li>
-
-		<li>
-		<?php echo get_category_parents( $postcat, true, '</li><li>' ); ?>
-		<a><?php the_title(); ?></a>
-		</li>
-	</ol>
-	<?php endforeach; ?>
-	<?php endif; ?>
-	</div>
-</div>
-
 <div class="container">
 <div class="contents">
 
-	<button type="button" id="neighbornav">
-		<i class="fa fa-bars"></i><span>NEIGHBORHOOD</span>
-	</button>
-
+<button type="button" id="neighbornav">
+	<i class="fa fa-bars"></i><span>NEIGHBORHOOD</span>
+</button>
 <?php
-$args=array(
-			'post_type'		=> 'post',/*投稿タイプ*/
-			'posts_per_page'=> '10',/*投稿表示数*/
-			'meta_key'		=> 'recommend',
-			'orderby'		=> 'meta_value_num',
-			'meta_query'	=> array(
-				'relation'		=> 'OR',
+	/*-------------------------------------------*/
+	/*  URLのパラメター(緯度と経度の位置情報)を取得
+	/*-------------------------------------------*/
+	$lat = (isset($_GET['lat'])) ? esc_html($_GET['lat']) : '';
+	$lng = (isset($_GET['lng'])) ? esc_html($_GET['lng']) : '';
+	/*-------------------------------------------*/
+	/*  距離チェック
+	/*-------------------------------------------*/
+	$myposts = get_posts( array(
+	    'post_type' => 'post',      // カスタム投稿タイプチェックイン
+	    'posts_per_page' => 10,     // 10件表示
+		'category' => -1,			// カテゴリが未分類の記事は非表示
+		'meta_key'		=> 'recommend',
+		'orderby'		=> 'meta_value_num',
+		'meta_query'	=> array(
+			'relation'		=> 'OR',
+			array(
+				'relation'		=> 'AND',
 				array(
-					'relation'		=> 'AND',
-					array(
-						'key'		=> 'eventclose',
-						'compare'	=> 'NOT EXISTS',
-					),
-					array(
-						'key'		=> 'eventopen',
-						'compare'	=> 'NOT EXISTS',
-					),
+					'key'		=> 'eventclose',
+					'compare'	=> 'NOT EXISTS',
 				),
 				array(
-					'relation'		=> 'AND',
-					array(
-						'key'		=> 'eventclose',
-						'compare'	=> 'NOT EXISTS',
-					),
-					array(
-						'key'		=> 'eventopen', //カスタムフィールドのイベント開催日欄
-						'value'		=> date_i18n( "Y/m/d" ), //イベント開催日を今日と比較
-						'compare'	=> '<=', //今日以前なら表示
-					),
-				),
-				array(
-					'relation'		=> 'AND',
-					array(
-						'key'		=> 'eventclose', //カスタムフィールドのイベント終了日欄
-						'value'		=> date_i18n( "Y/m/d" ), //イベント終了日を今日と比較
-						'compare'	=> '>=', // 今日以降なら表示
-					),
-					array(
-						'key'		=> 'eventopen',
-						'compare'	=> 'NOT EXISTS',
-					),
-				),
-				array(
-					'reration'		=> 'AND',
-					array(
-						'key'		=> 'eventclose', //カスタムフィールドのイベント終了日欄
-						'value'		=> date_i18n( "Y/m/d" ), //イベント終了日を今日と比較
-						'compare'	=> '>=', // 今日以降なら表示
-					),
-					array(
-						'key'		=> 'eventopen', //カスタムフィールドのイベント開催日欄
-						'value'		=> date_i18n( "Y/m/d" ), //イベント開催日を今日と比較
-						'compare'	=> '<=', //今日以前なら表示
-					),
+					'key'		=> 'eventopen',
+					'compare'	=> 'NOT EXISTS',
 				),
 			),
-			'paged'=>$paged
-		); ?>
-<?php $the_query = new WP_Query($args); ?>
+			array(
+				'relation'		=> 'AND',
+				array(
+					'key'		=> 'eventclose',
+					'compare'	=> 'NOT EXISTS',
+				),
+				array(
+					'key'		=> 'eventopen', //カスタムフィールドのイベント開催日欄
+					'value'		=> date_i18n( "Y/m/d" ), //イベント開催日を今日と比較
+					'compare'	=> '<=', //今日以前なら表示
+				),
+			),
+			array(
+				'relation'		=> 'AND',
+				array(
+					'key'		=> 'eventclose', //カスタムフィールドのイベント終了日欄
+					'value'		=> date_i18n( "Y/m/d" ), //イベント終了日を今日と比較
+					'compare'	=> '>=', // 今日以降なら表示
+				),
+				array(
+					'key'		=> 'eventopen',
+					'compare'	=> 'NOT EXISTS',
+				),
+			),
+			array(
+				'reration'		=> 'AND',
+				array(
+					'key'		=> 'eventclose', //カスタムフィールドのイベント終了日欄
+					'value'		=> date_i18n( "Y/m/d" ), //イベント終了日を今日と比較
+					'compare'	=> '>=', // 今日以降なら表示
+				),
+				array(
+					'key'		=> 'eventopen', //カスタムフィールドのイベント開催日欄
+					'value'		=> date_i18n( "Y/m/d" ), //イベント開催日を今日と比較
+					'compare'	=> '<=', //今日以前なら表示
+				),
+			),
+		),
+	) );
+	/*      現在地からイベント場所の距離を算出してデータに追加
+	/*-------------------------------------------*/
+	foreach ($myposts as $key => $spot_item) {
+		$address = esc_html( get_post_meta($spot_item->ID, 'address', true) );
+		$LatLng = strAddrToLatLng($address);
+		$spotLat = $LatLng['Lat'];
+		$spotLng = $LatLng['Lng'];
+	    if (($spotLat) && ($spotLng) && ($lat) && ($lng)) {
+	        $distanceLat = $spotLat - $lat;
+	        $distanceLng = $spotLng - $lng;
+	        // 距離の算出　pow = 乗算 / sqrt = 平方根
+	        $distance = sqrt(pow( $distanceLat ,2) + pow( $distanceLng ,2));
+	        // 並び替え用の数値として距離「distance」を追加
+//			update_post_meta($spot_item->ID, 'distance', $distance);
+	        $myposts[$key]->distance = $distance;
+	    } else {
+			$myposts[$key]->distance = 0;
+		}
+	}
+	/*      距離で並び替えるという比較関数を定義
+	/*-------------------------------------------*/
+	function itemsort_by_distance( $a , $b){
+		//距離を比較
+		$myposts = strcmp( $a->distance , $b->distance );
+		return $myposts;
+	}
+	/*      比較関数にそって並び替え
+	/*-------------------------------------------*/
+	usort( $myposts , "itemsort_by_distance" );
 
-	<?php if($the_query->have_posts()): while($the_query->have_posts()):
-	$the_query->the_post(); ?>
+	/*-------------------------------------------*/
+	/*  並び替えたイベント情報を出力
+	/*-------------------------------------------*/
+?>
+	<?php foreach ($myposts as $post) : setup_postdata($post); ?>
+		<?php get_template_part( 'gaiyou', 'medium' ); ?>
+	<?php endforeach; ?>
 
-	<?php get_template_part( 'gaiyou', 'medium' ); ?>
-
-	<?php endwhile; endif; ?>
-
-	<div class="pagination pagination-index">
-	<?php echo paginate_links( array( 'type' => 'list',
-							'prev_text' => '&laquo;',
-							'next_text' => '&raquo;',
-							'total'		=> $the_query->max_num_pages
-							 ) ); ?>
-	</div>
 	<?php wp_reset_postdata(); ?>
-</div>
 
+	<div class="share">
+	<ul>
+	<li><a href="https://twitter.com/intent/tweet?text=<?php echo urlencode( get_the_title() . ' - ' . get_bloginfo('name') ); ?>&amp;url=<?php echo urlencode( get_permalink() ); ?>&amp;via=sagaswhat"
+	onclick="window.open(this.href, 'SNS', 'width=500, height=300, menubar=no, toolbar=no, scrollbars=yes'); return false;" class="share-tw">
+    	<i class="fa fa-twitter"></i>
+    	<span>share&nbsp;by</span>&nbsp;Twitter
+	</a></li>
+	<li><a href="http://www.facebook.com/share.php?u=<?php echo urlencode( get_permalink() ); ?>"
+	onclick="window.open(this.href, 'SNS', 'width=500, height=500, menubar=no, toolbar=no, scrollbars=yes'); return false;" class="share-fb">
+    	<i class="fa fa-facebook"></i>
+    	<span>share&nbsp;by</span>&nbsp;Facebook
+	</a></li>
+	<li><a href="https://plus.google.com/share?url=<?php echo urlencode( get_permalink() ); ?>"
+	onclick="window.open(this.href, 'SNS', 'width=500, height=500, menubar=no, toolbar=no, scrollbars=yes'); return false;" class="share-gp">
+    	<i class="fa fa-google-plus"></i>
+    	<span>share&nbsp;by</span>&nbsp;Google+
+	</a></li>
+	</ul>
+	</div><!-- end share -->
+</div><!-- end contents -->
 <div class="sub">
 	<?php get_sidebar(); ?>
-</div>
-</div>
+</div><!-- end sub -->
+</div><!-- end container -->
 
 <?php get_footer(); ?>
