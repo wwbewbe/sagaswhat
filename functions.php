fc942f47b7783c1014b593e152dc142ec81f0653
wwbewbe <what.wbewbe@gmail.com>
2016-06-09 10:49:10 +0900

    add excerpt character length for english in function.php

--- functions.php
+++ functions.php
@@ -6,6 +6,12 @@ function my_length($length) {
 }
 add_filter('excerpt_mblength', 'my_length');
 
+//概要（抜粋）の英語文字数
+function en_length($length) {
+	return 20;
+}
+add_filter('excerpt_length', 'en_length');
+
 //概要（抜粋）の省略記号
 function my_more($more) {
 	return '…';
