81780ee7150036a6cce7ff1543eaae4ed7acb75b
wwbewbe <what.wbewbe@gmail.com>
2017-01-30 00:46:07 +0900

    add trend topics on sidebar

--- sidebar.php
+++ sidebar.php
@@ -39,6 +39,37 @@ if( $myposts ): ?>
 <?php wp_reset_postdata();
 endif; ?>
 
+<aside class="mymenu mymenu-feature">
+<h2><?php echo esc_html__('Trend Topics', 'SagasWhat'); ?></h2>
+<ul>
+	<?php
+	$args=array(
+				'post_type'		=> 'sw_trend',	//表示するカスタム投稿タイプ(sw_trend)
+				'posts_per_page'=> '3',       //リスト数を指定
+			);
+	$the_query = new WP_Query($args); ?>
+
+	<?php if($the_query->have_posts()): while($the_query->have_posts()):
+	$the_query->the_post(); ?>
+		<li><a href="<?php the_permalink(); ?>">
+		<div class="thumb" style="background-image: url(<?php echo mythumb( 'medium' ); ?>)"></div>
+		<div class="text">
+		<?php the_title(); ?>
+
+		<div class="kiji-date">
+		<i class="fa fa-pencil fa-fw"></i>
+		<time datetime="<?php echo get_the_date( 'Y-m-d' ); ?>">
+			<?php echo get_the_date( __('M j, Y', 'SagasWhat') ); ?>
+		</time>
+		</div>
+
+		</div>
+		</a></li>
+	<?php endwhile; endif; ?>
+</ul>
+</aside>
+<?php wp_reset_postdata(); ?>
+
 <?php if (is_single()) {
 	dynamic_sidebar( 'submenu-post' );
 } else {
