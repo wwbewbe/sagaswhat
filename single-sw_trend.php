9939b2cdcad76994a901a4246404d8b711972410
wwbewbe <what.wbewbe@gmail.com>
2017-01-31 21:14:07 +0900

    add bread list on Trend Topicis

--- single-sw_trend.php
+++ single-sw_trend.php
@@ -4,6 +4,20 @@
 	<aside class="mymenu-adsense">
 	<?php echo get_adsense(true); ?>
 	</aside>
+	<div class="bread">
+	<ol>
+		<li><a href="<?php echo home_url(); ?>">
+		<i class="fa fa-home"></i><span>TOP</span>
+		</a></li>
+
+		<li>
+		<a href="<?php echo esc_url( get_permalink( get_page_by_title( esc_html__('Trend Topics', 'SagasWhat') ) ) ); ?>"><?php echo esc_html__('Trend Topics', 'SagasWhat'); ?></a>
+		</li>
+		<li>
+		<a><span>&gt;</span></a>
+		</li>
+	</ol>
+	</div>
 </div>
 
 <div class="container">
@@ -56,6 +70,48 @@
 		'after' => '</li></ul></div>',
 		'pagelink' => '<span>%</span>'
 	) ); ?>
+
+	<script type="text/javascript">
+	jQuery(function() {
+	    jQuery(".carousel-rel-list").jCarouselLite({
+			btnNext: ".next",
+			btnPrev: ".prev",
+			visible: 4,
+			speed: 100,
+			circular: false,
+		});
+	});
+	</script>
+
+	<?php
+	$myposts = get_posts( array(
+		'post_type'		=> 'sw_trend',
+		'posts_per_page'=> '-1',
+	) );
+	if( $myposts ): ?>
+	<aside class="mymenu mymenu-thumb mymenu-related" style="margin-bottom:10px;">
+	<h2><?php echo esc_html(__('Other Topics', 'SagasWhat')); ?></h2>
+	<div class="carousel-rel">
+	<a href="#" class="prev"><i class="fa fa-arrow-left"></i><?php echo esc_html__('Prev', 'SagasWhat'); ?></a>
+    <a href="#" class="next"><?php echo esc_html__('Next', 'SagasWhat'); ?><i class="fa fa-arrow-right"></i></a>
+	<div class="carousel-rel-list">
+	<ul>
+		<?php foreach($myposts as $post):
+		setup_postdata($post); ?>
+		<li><a href="<?php the_permalink(); ?>">
+		<div class="thumb" style="background-image: url(<?php echo mythumb( 'full' ); ?>)">
+		</div>
+		<div class="text">
+		<?php the_title(); ?>
+		</div>
+		</a></li>
+		<?php endforeach; ?>
+	</ul>
+	</div>
+	</div>
+	</aside>
+	<?php wp_reset_postdata();
+	endif; ?>
 
 	<?php get_template_part( 'nearby', 'events' ); //Nearby Events list function ?>
 	<?php get_template_part( 'nearby', 'tic' ); //Nearby TIC list function ?>
