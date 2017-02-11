e31f50dcd5198363711ab2990cb8494379726e9f
wwbewbe <what.wbewbe@gmail.com>
2017-02-11 18:17:15 +0900

    fix custom fields edtor design

--- inc/custom-edit.php
+++ inc/custom-edit.php
@@ -81,22 +81,24 @@ function rest_setting( $post ) {
 	 * Use get_post_meta() to retrieve an existing value
 	 * from the database and use the value for the form.
 	 */
+	// table
+	echo '<table style="width:100%;text-align:left;"><tbody>';
 	//textarea for Resting Spot name
 	$value = get_post_meta(
 		$post->ID,	//post ID
 		'eventname',		//Custom Field Key
 		true		//true:単一文字列, false:複数配列
 	);
-	echo '<dl>';
-	echo   '<dt>';
+	echo '<tr>';
+	echo   '<th style="width:10%;">';
 	echo     '<label for="Eventname">';
 	echo     '休憩スポット名';
 	echo     '</label>';
-	echo   '</dt>';
-	echo   '<dd>';
+	echo   '</th>';
+	echo   '<td style="width:90%;">';
 	echo     '<textarea id="Eventname" name="eventname" style="width:100%">'.$value.'</textarea>';
-	echo   '</dd>';
-	echo '</dl>';
+	echo   '</td>';
+	echo '</tr>';
 
 	//textarea for Nearest Station name
 	$value = get_post_meta(
@@ -104,16 +106,16 @@ function rest_setting( $post ) {
 		'venue',		//Custom Field Key
 		true		//true:単一文字列, false:複数配列
 	);
-	echo '<dl>';
-	echo   '<dt>';
+	echo '<tr>';
+	echo   '<th>';
 	echo     '<label for="Venue">';
 	echo     '最寄り駅';
 	echo     '</label>';
-	echo   '</dt>';
-	echo   '<dd>';
+	echo   '</th>';
+	echo   '<td>';
 	echo     '<textarea id="Venue" name="venue" style="width:100%">'.$value.'</textarea>';
-	echo   '</dd>';
-	echo '</dl>';
+	echo   '</td>';
+	echo '</tr>';
 
 	//textarea for SwhoAddress
 	$value = get_post_meta(
@@ -121,16 +123,16 @@ function rest_setting( $post ) {
 		'showaddress',		//Custom Field Key
 		true		//true:単一文字列, false:複数配列
 	);
-	echo '<dl>';
-	echo   '<dt>';
+	echo '<tr>';
+	echo   '<th>';
 	echo     '<label for="ShowAddress">';
 	echo     '表示住所';
 	echo     '</label>';
-	echo   '</dt>';
-	echo   '<dd>';
+	echo   '</th>';
+	echo   '<td>';
 	echo     '<textarea id="ShowAddress" name="showaddress" style="width:100%">'.$value.'</textarea>';
-	echo   '</dd>';
-	echo '</dl>';
+	echo   '</td>';
+	echo '</tr>';
 
 	//textarea for Address
 	$value = get_post_meta(
@@ -138,16 +140,16 @@ function rest_setting( $post ) {
 		'address',		//Custom Field Key
 		true		//true:単一文字列, false:複数配列
 	);
-	echo '<dl>';
-	echo   '<dt>';
+	echo '<tr>';
+	echo   '<th>';
 	echo     '<label for="Address">';
 	echo     '住所';
 	echo     '</label>';
-	echo   '</dt>';
-	echo   '<dd>';
+	echo   '</th>';
+	echo   '<td>';
 	echo     '<textarea id="Address" name="address" style="width:100%">'.$value.'</textarea>';
-	echo   '</dd>';
-	echo '</dl>';
+	echo   '</td>';
+	echo '</tr>';
 
 	//textarea for Open Hours
 	$value = get_post_meta(
@@ -155,16 +157,16 @@ function rest_setting( $post ) {
 		'bizhours',		//Custom Field Key
 		true		//true:単一文字列, false:複数配列
 	);
-	echo '<dl>';
-	echo   '<dt>';
+	echo '<tr>';
+	echo   '<th>';
 	echo     '<label for="BizHours">';
 	echo     '開園時間';
 	echo     '</label>';
-	echo   '</dt>';
-	echo   '<dd>';
+	echo   '</th>';
+	echo   '<td>';
 	echo     '<textarea id="BizHours" name="bizhours" style="width:100%">'.$value.'</textarea>';
-	echo   '</dd>';
-	echo '</dl>';
+	echo   '</td>';
+	echo '</tr>';
 
 	//textarea for Open Hours
 	$value = get_post_meta(
@@ -172,16 +174,16 @@ function rest_setting( $post ) {
 		'url',		//Custom Field Key
 		true		//true:単一文字列, false:複数配列
 	);
-	echo '<dl>';
-	echo   '<dt>';
+	echo '<tr>';
+	echo   '<th>';
 	echo     '<label for="URL">';
 	echo     'URL';
 	echo     '</label>';
-	echo   '</dt>';
-	echo   '<dd>';
+	echo   '</th>';
+	echo   '<td>';
 	echo     '<textarea id="URL" name="url" style="width:100%">'.$value.'</textarea>';
-	echo   '</dd>';
-	echo '</dl>';
+	echo   '</td>';
+	echo '</tr>';
 
 	//textarea for Open Hours
 	$value = get_post_meta(
@@ -189,16 +191,16 @@ function rest_setting( $post ) {
 		'recommend',		//Custom Field Key
 		true		//true:単一文字列, false:複数配列
 	);
-	echo '<dl>';
-	echo   '<dt>';
+	echo '<tr>';
+	echo   '<th>';
 	echo     '<label for="Recommend">';
 	echo     'おすすめ度';
 	echo     '</label>';
-	echo   '</dt>';
-	echo   '<dd>';
+	echo   '</th>';
+	echo   '<td>';
 	echo     '<textarea id="Recommend" name="recommend" style="width:100%">'.$value.'</textarea>';
-	echo   '</dd>';
-	echo '</dl>';
+	echo   '</td>';
+	echo '</tr>';
 
 	//radio button for City
 	$value = get_post_meta(
@@ -209,11 +211,11 @@ function rest_setting( $post ) {
 	$list = array(
 		 1 => '千代田区', 2 => '中央区', 3 => '港区', 4 => '新宿区', 5 => '文京区', 6 => '台東区', 7 => '隅田区', 8 => '江東区', 9 => '品川区', 10 => '目黒区', 11 => '大田区', 12 => '世田谷区', 13 => '渋谷区', 14 => '中野区', 15 => '杉並区', 16 => '豊島区', 17 => '北区', 18 => '荒川区', 19 => '板橋区', 20 => '練馬区', 21 => '足立区', 22 => '葛飾区', 23 => '江戸川区', 24 => '武蔵野市', 25 => '府中市', 26 => '調布市', 27 => '小金井市',
 	);
-	echo '<dl>';
-	echo   '<dt>';
+	echo '<tr>';
+	echo   '<th>';
 	echo   '市区町村';
-	echo   '</dt>';
-	echo   '<dd>';
+	echo   '</th>';
+	echo   '<td>';
 	echo     '<input name="city" value="0" type="hidden">';
 	echo     '<ul>';
 	foreach ($list as $_id => $_name) {
@@ -229,8 +231,8 @@ function rest_setting( $post ) {
 		echo   '</li>';
 	}
 	echo     '</ul>';
-	echo   '</dd>';
-	echo '</dl>';
+	echo   '</td>';
+	echo '</tr>';
 	echo '<p style="clear:left;"></p>';
 
     //checkbox for Facility
@@ -244,11 +246,11 @@ function rest_setting( $post ) {
         2 => '売店・カフェ・自販機',
         3 => '草地広場',
     );
-    echo '<dl>';
-    echo   '<dt>';
+    echo '<tr>';
+    echo   '<th>';
     echo   '設備';
-    echo   '</dt>';
-    echo   '<dd>';
+    echo   '</th>';
+    echo   '<td>';
     echo     '<ul>';
     foreach ($list as $_id => $_name) {
         echo   '<li style="float:left; margin-right:10px">';
@@ -263,8 +265,8 @@ function rest_setting( $post ) {
         echo   '</li>';
     }
     echo     '</ul>';
-    echo   '</dd>';
-    echo '</dl>';
+    echo   '</td>';
+    echo '</tr>';
 	echo '<p style="clear:left;"></p>';
 
     //radio button for How big
@@ -278,11 +280,11 @@ function rest_setting( $post ) {
         2 => '中',
         3 => '大',
     );
-    echo '<dl>';
-    echo   '<dt>';
+    echo '<tr>';
+    echo   '<th>';
     echo   '広さ';
-    echo   '</dt>';
-    echo   '<dd>';
+    echo   '</th>';
+    echo   '<td>';
     echo     '<input name="howbig" value="0" type="hidden">';
     echo     '<ul>';
     foreach ($list as $_id => $_name) {
@@ -298,10 +300,11 @@ function rest_setting( $post ) {
         echo   '</li>';
     }
     echo     '</ul>';
-    echo   '</dd>';
-    echo '</dl>';
+    echo   '</td>';
+    echo '</tr>';
 	echo '<p style="clear:left;"></p>';
 
+	echo '</table></tbody>';
 }
 /**
  * カスタムフィールドの保存処理
