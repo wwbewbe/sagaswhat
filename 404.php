e637c5d3051fcbcf70280fc6d2c36cf194f01752
wwbewbe <what.wbewbe@gmail.com>
2017-04-21 14:06:41 +0900

    use Beautify to fix format

--- 404.php
+++ 404.php
@@ -1,19 +1,21 @@
 <?php get_header(); ?>
 
 <div class="container">
-<div class="contents">
-	<div class="not-find">
-		<h1>404</h1>
-	<div class="msg">
-		<h2><?php echo esc_html(__('Sorry, the page you are looking for cannot be found.', 'SagasWhat')); ?></h2>
-		<?php echo esc_html(__('Please visit homepage or try searching.', 'SagasWhat')); ?>
-		<?php get_search_form(); // 検索フォームを出力 ?>
+	<div class="contents">
+		<div class="not-find">
+			<h1>404</h1>
+			<div class="msg">
+				<h2>
+					<?php echo esc_html(__('Sorry, the page you are looking for cannot be found.', 'SagasWhat')); ?>
+				</h2>
+				<?php echo esc_html(__('Please visit homepage or try searching.', 'SagasWhat')); ?>
+				<?php get_search_form(); // 検索フォームを出力 ?>
+			</div>
+		</div>
 	</div>
-	</div>
-</div>
 
-<div class="sub">
-</div>
+	<div class="sub">
+	</div>
 </div>
 
 <?php get_footer(); ?>
--- category.php
+++ category.php
@@ -1,51 +1,55 @@
 <?php get_header(); ?>
 
 <div class="container">
-<div class="contents">
+	<div class="contents">
 
-	<h1><?php single_term_title(); ?></h1>
+		<h1>
+			<?php single_term_title(); ?>
+		</h1>
 
-	<?php if(have_posts()): while(have_posts()):
-	the_post(); ?>
+		<?php if(have_posts()): while(have_posts()):
+		the_post(); ?>
 
-	<?php if (get_post_type() == 'sw_trend') : ?>
-		<?php get_template_part( 'gaiyou', 'trends' ); ?>
-	<?php else : ?>
+		<?php if (get_post_type() == 'sw_trend') : ?>
+			<?php get_template_part( 'gaiyou', 'trends' ); ?>
+		<?php else : ?>
 		<?php if (is_category('tourist-info-center')) {
 			get_template_part( 'gaiyou', 'medium-tic' );
 		} else {
 			get_template_part( 'gaiyou', 'medium' );
 		} ?>
-	<?php endif; ?>
+		<?php endif; ?>
 
-	<?php endwhile; endif; ?>
+		<?php endwhile; endif; ?>
 
 
-	<div class="pagination pagination-index">
-	<?php echo paginate_links( array(
-		'type' => 'list',
-		'prev_text' => '&laquo;',
-		'next_text' => '&raquo;'
-	) ); ?>
+		<div class="pagination pagination-index">
+			<?php echo paginate_links( array(
+				'type' => 'list',
+				'prev_text' => '&laquo;',
+				'next_text' => '&raquo;'
+			) ); ?>
+		</div>
+
+		<?php if (is_category('tourist-info-center')) : ?>
+			<div class="tic-comment-end">
+				<?php echo get_option('tic-comment-end'); ?>
+			</div>
+		<?php endif; ?>
+
+		<aside class="mymenu-adsense">
+			<?php echo get_adsense(); ?>
+		</aside>
+
+		<?php if (function_exists('wpfp_list_favorite_posts')) {
+			get_template_part( 'favorite', 'events' );
+		} //Favorite Events list function?>
+
 	</div>
 
-	<?php if (is_category('tourist-info-center')) : ?>
-		<div class="tic-comment-end"><?php echo get_option('tic-comment-end'); ?></div>
-	<?php endif; ?>
-
-	<aside class="mymenu-adsense">
-	<?php echo get_adsense(); ?>
-	</aside>
-
-	<?php if (function_exists('wpfp_list_favorite_posts')) {
-		get_template_part( 'favorite', 'events' );
-	} //Favorite Events list function?>
-
-</div>
-
-<div class="sub">
-	<?php get_sidebar(); ?>
-</div>
+	<div class="sub">
+		<?php get_sidebar(); ?>
+	</div>
 </div>
 
 <?php get_footer(); ?>
